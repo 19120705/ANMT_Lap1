d490cc8929f0294acb75785a796d8e1be5c253e0
19120705 <tranviettuan2k1@gmail.com>
2022-10-22 16:56:29 +0700

    commit 5

--- app.py
+++ app.py
@@ -5,13 +5,9 @@ from flask_login import UserMixin, login_user, LoginManager, login_required, log
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import InputRequired, Length, ValidationError
-import hashlib
+
 from Crypto.Hash import SHA256
 from Crypto.Random import get_random_bytes
-# from .encrypt import encrypt
-
-
-
 from Crypto import Random
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES, PKCS1_OAEP
@@ -19,74 +15,7 @@ from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
 # from bs4 import Tag
 import hashlib
 import binascii
-
-
-def generate_RSA(bits=2048):
-    '''
-    Generate an RSA keypair with an exponent of 65537 in PEM format
-    param: bits The key length in bits
-    Return private key and public key
-    '''
-    #from Crypto.PublicKey import RSA 
-    new_key = RSA.generate(bits, e=65537) 
-    public_key = new_key.publickey().exportKey("PEM") 
-    private_key = new_key.exportKey("PEM") 
-    return public_key,private_key
-
-#ENCRYPT K_PRI WITH AES
-def AES_encrypt(key,message):
-    cipher=AES.new(key, AES.MODE_EAX)
-    nonce = cipher.nonce
-    #ciphertext,tag =cipher.encrypt_and_digest(message.encode('ascii'))
-    ciphertext,tag =cipher.encrypt_and_digest(message)
-    return nonce, ciphertext, tag
-
-def AES_decrypt(key, nonce, ciphertext, tag):
-    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce )
-    plaintext = cipher.decrypt(ciphertext)
-    try: 
-        cipher.verify(tag)
-        return plaintext
-    except:
-        return False
-
-
-#Encrypt file
-def test_encypt(session_key,enc_session_key,filename):
-    with open(filename, 'rb') as f:
-        data= f.read()
-    file_out = open(filename+"_encrypt", "wb")
-    cipher_aes = AES.new(session_key, AES.MODE_EAX)
-    ciphertext, tag = cipher_aes.encrypt_and_digest(data)
-    [ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
-    file_out.close()
-
-def test_decrypt(private_key,filename):
-    file_in = open(filename+"_encrypt", "rb")
-    enc_session_key, nonce, tag, ciphertext = [file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
-    cipher_rsa = PKCS1_OAEP.new(private_key)
-    session_key = cipher_rsa.decrypt(enc_session_key)
-
-    # Decrypt the data with the AES session key
-    cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
-    data = cipher_aes.decrypt_and_verify(ciphertext, tag)
-    with open(filename+"_decrypt", 'wb') as out:
-        out.write(data)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+from encrypt import generate_RSA, AES_encrypt, AES_decrypt, test_encypt, test_decrypt, sign_file, verify_file
 
 
 
@@ -200,7 +129,6 @@ def logout():
 @ app.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegisterForm()
-
     if form.validate_on_submit():
         salt= get_random_bytes(32)
         password = request.form["password"]
@@ -227,51 +155,24 @@ def register():
 @ app.route('/uploadfile', methods=['GET', 'POST'])
 def upload():
     return render_template("uploadfile.html")  
-
-
-
-
-
-
-
 @app.route('/success', methods = ['GET', 'POST'])  
 def success():  
     if request.method == 'POST':
-
         session_key = get_random_bytes(16)
         #print(session_key)
-
         # Encrypt the session key with the public RSA key:
         profile = ProfileUser.query.filter_by(email=current_user.email).first()
         aes = AESKey.query.filter_by(email=current_user.email).first()
         recipient_key = RSA.import_key(profile.kpublic)
         cipher_rsa = PKCS1_OAEP.new(recipient_key)
         enc_session_key = cipher_rsa.encrypt(session_key)
-
         f = request.files['file']  
         f.save(f.filename)
-
         test_encypt(session_key,enc_session_key,f.filename)
-
         private_key = AES_decrypt(current_user.password, aes.nonce, profile.kprivate, aes.tag)
         private_key_real = RSA.import_key(private_key)
         test_decrypt(private_key_real,f.filename)
-
         return render_template("success.html", name = f.filename)  
-
-
-
-
-def sign_file(filename,private_key):
-    with open(filename, 'rb') as f:
-        msg_b = f.read()
-    #msg = b'A message for signing'
-    hash = SHA256.new(msg_b)
-    signer = PKCS115_SigScheme(private_key)
-    signature = signer.sign(hash)
-    file_out = open(filename+".sig", "wb")
-    [ file_out.write(x) for x in (signature,msg_b) ]
-    file_out.close()
 
 
 
@@ -294,24 +195,6 @@ def check_sig():
     return render_template("checksig.html") 
 
 
-def verify_file(sign_filename,filename,public_key):
-    #msg = b'hello vn 123 @'
-    with open(filename, 'rb') as f:
-        msg= f.read()
-    
-    #print(msg)
-    
-    file_in = open(sign_filename, "rb")
-    signature,msg_2 = [file_in.read(x) for x in (256, -1) ]
-    #print(msg_2)
-    #print(signature)
-    hash = SHA256.new(msg)
-    signer = PKCS115_SigScheme(public_key)
-    try:
-        signer.verify(hash, signature)
-        return True
-    except:
-        return False
 
 
 
--- encrypt.py
+++ encrypt.py
@@ -1,8 +1,14 @@
+
+from Crypto.Hash import SHA256
+from Crypto.Random import get_random_bytes
+# from .encrypt import encrypt
 from Crypto import Random
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import AES, PKCS1_OAEP
+from Crypto.Signature.pkcs1_15 import PKCS115_SigScheme
 # from bs4 import Tag
 import hashlib
+import binascii
 
 
 def generate_RSA(bits=2048):
@@ -17,3 +23,73 @@ def generate_RSA(bits=2048):
     private_key = new_key.exportKey("PEM") 
     return public_key,private_key
 
+#ENCRYPT K_PRI WITH AES
+def AES_encrypt(key,message):
+    cipher=AES.new(key, AES.MODE_EAX)
+    nonce = cipher.nonce
+    #ciphertext,tag =cipher.encrypt_and_digest(message.encode('ascii'))
+    ciphertext,tag =cipher.encrypt_and_digest(message)
+    return nonce, ciphertext, tag
+
+def AES_decrypt(key, nonce, ciphertext, tag):
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce )
+    plaintext = cipher.decrypt(ciphertext)
+    try: 
+        cipher.verify(tag)
+        return plaintext
+    except:
+        return False
+
+
+#Encrypt file
+def test_encypt(session_key,enc_session_key,filename):
+    with open(filename, 'rb') as f:
+        data= f.read()
+    file_out = open(filename+"_encrypt", "wb")
+    cipher_aes = AES.new(session_key, AES.MODE_EAX)
+    ciphertext, tag = cipher_aes.encrypt_and_digest(data)
+    [ file_out.write(x) for x in (enc_session_key, cipher_aes.nonce, tag, ciphertext) ]
+    file_out.close()
+
+def test_decrypt(private_key,filename):
+    file_in = open(filename+"_encrypt", "rb")
+    enc_session_key, nonce, tag, ciphertext = [file_in.read(x) for x in (private_key.size_in_bytes(), 16, 16, -1) ]
+    cipher_rsa = PKCS1_OAEP.new(private_key)
+    session_key = cipher_rsa.decrypt(enc_session_key)
+
+    # Decrypt the data with the AES session key
+    cipher_aes = AES.new(session_key, AES.MODE_EAX, nonce)
+    data = cipher_aes.decrypt_and_verify(ciphertext, tag)
+    with open(filename+"_decrypt", 'wb') as out:
+        out.write(data)
+
+def sign_file(filename,private_key):
+    with open(filename, 'rb') as f:
+        msg_b = f.read()
+    #msg = b'A message for signing'
+    hash = SHA256.new(msg_b)
+    signer = PKCS115_SigScheme(private_key)
+    signature = signer.sign(hash)
+    file_out = open(filename+".sig", "wb")
+    [ file_out.write(x) for x in (signature,msg_b) ]
+    file_out.close()
+    
+
+def verify_file(sign_filename,filename,public_key):
+    #msg = b'hello vn 123 @'
+    with open(filename, 'rb') as f:
+        msg= f.read()
+    
+    #print(msg)
+    
+    file_in = open(sign_filename, "rb")
+    signature,msg_2 = [file_in.read(x) for x in (256, -1) ]
+    #print(msg_2)
+    #print(signature)
+    hash = SHA256.new(msg)
+    signer = PKCS115_SigScheme(public_key)
+    try:
+        signer.verify(hash, signature)
+        return True
+    except:
+        return False
